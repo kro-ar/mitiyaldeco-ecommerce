5941226eebb7959a45b814aefef6f3158c2c5448
Caro Arias <arias.carolina.ayelen@gmail.com>
2022-09-28 17:23:34 -0300

    Se ven errores en conexion a firebase

--- src/componentes/Productos/ItemListContainer.jsx
+++ src/componentes/Productos/ItemListContainer.jsx
@@ -2,28 +2,31 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import products from "../../utils/dataBase";
 import ItemList from "./ItemList";
-
+import { collection, getDocs } from "firebase/firestore";
+import {db} from "../../utils/firebaseConfig"
 
 const ItemListContainer = () => {
   const [listProducts, setListProducts] = useState([]);
 
-  const {categoria} = useParams();
+  const { categoria } = useParams();
   console.log(categoria);
 
-  const customFetch =(items) =>
-{
-    return new Promise ((resolve , reject)=>{
-        
-    setTimeout(() => {
-      if(categoria){
-        resolve(products.filter((item)=> item.category == categoria))
-      } else  resolve(items)
-    },)
-})
-
-}
-  useEffect(() => {
-    customFetch(products).then((data) => setListProducts(data));
+  const customFetch = (items) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (categoria) {
+          resolve(products.filter((item) => item.category == categoria));
+        } else resolve(items);
+      });
+    });
+  };
+  useEffect(async () => {
+    const querySnapshot = await getDocs(collection(db, "products"));
+    const dataFromFirebase = querySnapshot.docs.map(item=>({
+      id:item.id,
+      ...item.data()
+    }))
+   console.log(dataFromFirebase)
   }, [categoria]);
 
   return (
