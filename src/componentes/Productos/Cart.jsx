a39ce3ea19e36e69842188ff7eefc581f354b626
Caro Arias <arias.carolina.ayelen@gmail.com>
2022-09-29 00:01:53 -0300

    Se añade nuevos productos y se inicia orden de compra

--- src/componentes/Productos/Cart.jsx
+++ src/componentes/Productos/Cart.jsx
@@ -2,36 +2,88 @@ import { useContext } from "react";
 import { CartContext } from "./CartContext";
 import { Link } from "react-router-dom";
 import ItemCart from "./ItemCart";
+import { serverTimestamp, doc, setDoc, collection } from "firebase/firestore";
+import { db } from "../../utils/firebaseConfig";
 
-const Cart = ()=>{
-     //Se obtiene el contenido de la variabe global
-    const ctx = useContext(CartContext)
-    return(
-        <main>
-            <h1 className="text-center">Esta es su orden 🛒</h1>
-            <section className="row mx-0">
-                <div className="col-12 row">
-                    <Link to="/" ><button type="button" className="btn py-0 " >Continuar comprando</button></Link>
-                            
-                    
-                    <div className="col-6 d-flex justify-content-end">
-                        <button type="button" className="btn py-0 " onClick={ctx.clear}>Eliminar compra</button>
-                    </div>
-                </div>
-                <div className="col-8 ">
-                    {
-                        //Se mapea el contenido y se imprime en la lista
-                        ctx.cartList.map(item=> <ItemCart product={item} key={item.id}/>)
-                    }
-                </div>
-                <div className="row col-3">
-                    <h3 className="text-center ">Su orden</h3>
-                    <p className="col-6">SubTotal:<br/>IVA:<br/>Descuento:<br/><strong>Total:</strong></p>
-                    <p className="col-6">${ctx.totals}<br/>${ctx.totals*0.05 }<br/>${ctx.totals*0.05 }<br/><strong>${ctx.totals}</strong></p>
-                </div>
-            </section>
-        </main>
-    )
-}
+const Cart = () => {
+  //Se obtiene el contenido de la variabe global
+  const ctx = useContext(CartContext);
+  const createOrder = async () => {
+    const itemsForDB = ctx.cartList.map((item) => ({
+      id: item.id,
+      nombre: item.tittle,
+      precio: item.price,
+      // cantidad:item.quantity + quantity
+    }));
+    let order = {
+      buyer: {
+        nombre: "Comprador Prueba",
+        email: "falsomail@gmail.com",
+        tel: "1123456",
+      },
+      items: itemsForDB,
+      date: serverTimestamp(),
+      total: ctx.totals,
+    };
+    //se crea orders ,en la base de datos
+    const newOrderRef = doc(collection(db, "orders"));
+    await setDoc(newOrderRef, order);
+    alert(
+      "Su orden ha sido procesada! Su numero de orden es " + newOrderRef.id
+    );
+  };
+  return (
+    <main>
+      <h1 className="text-center">Esta es su orden 🛒</h1>
+      <section className="row mx-0">
+        <div className="col-12 row">
+          <Link to="/">
+            <button type="button" className="btn py-0 ">
+              Continuar comprando
+            </button>
+          </Link>
+
+          <div className="col-6 d-flex justify-content-end">
+            <button type="button" className="btn py-0 " onClick={ctx.clear}>
+              Eliminar compra
+            </button>
+          </div>
+        </div>
+        <div className="col-8 ">
+          {
+            //Se mapea el contenido y se imprime en la lista
+            ctx.cartList.map((item) => (
+              <ItemCart product={item} key={item.id} />
+            ))
+          }
+        </div>
+        <div className="row col-3">
+          <h3 className="text-center ">Su orden</h3>
+          <p className="col-6">
+            SubTotal:
+            <br />
+            IVA:
+            <br />
+            Descuento:
+            <br />
+            <strong>Total:</strong>
+          </p>
+          <p className="col-6">
+            ${ctx.totals}
+            <br />${ctx.totals * 0.05}
+            <br />${ctx.totals * 0.05}
+            <br />
+            <strong>${ctx.totals}</strong>
+          </p>
+          <div className="text-center">
+            <button onClick={createOrder} type="button" className="btn p-2 ">
+              Finalizar Compra
+            </button>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+};
 
 export default Cart;
--- src/componentes/Productos/CartContext.jsx
+++ src/componentes/Productos/CartContext.jsx
@@ -1,65 +1,82 @@
-import React, {createContext, useState } from "react"
+import React, { createContext, useState } from "react";
 
 export const CartContext = createContext();
 
-const CartContextProvider = ({children})=>{
-    const [cartList, setCartList] = useState([])
-    const [totals , settotals] = useState(0)
-    //Funciones globales
-    const addItem = (item, quantity)=>{ // agregar cierta cantidad de un ítem al carrito
-        if(isInCart(item.id)){
-            let array = cartList.map(element=>(
-                (element.id!==item.id )? 
-                element:
-                {
-                    id:element.id,
-                    tittle:element.tittle,
-                    category: element.category,
-                    img : element.img,
-                    stock:element.stock,
-                    price:element.price,
-                    detail:element.detail,
-                    quantity : element.quantity+quantity,
-                    key:element.id
-                }
-            ))
-            console.log(array)
-            setCartList(array)
-        }else{  
-            item.quantity=quantity;
-            setCartList([...cartList,item]);
-        }
-        costTotal([...cartList,item])
+const CartContextProvider = ({ children }) => {
+  const [cartList, setCartList] = useState([]);
+  const [totals, settotals] = useState(0);
+  //Funciones globales
+  const addItem = (item, quantity) => {
+    // agregar cierta cantidad de un ítem al carrito
+    if (isInCart(item.id)) {
+      let array = cartList.map((element) =>
+        element.id !== item.id
+          ? element
+          : {
+              id: element.id,
+              tittle: element.tittle,
+              category: element.category,
+              img: element.img,
+              stock: element.stock,
+              price: element.price,
+              detail: element.detail,
+              quantity: element.quantity + quantity,
+              key: element.id,
+            }
+      );
+      console.log(array);
+      setCartList(array);
+    } else {
+      item.quantity = quantity;
+      setCartList([...cartList, item]);
     }
-    const removeItem = (itemId) =>{ // Remover un item del cart por usando su id
-        setCartList(cartList.filter(item=> item.id !== itemId));
-        costTotal(cartList.filter(item=> item.id !== itemId))
+    costTotal([...cartList, item]);
+  };
+  const removeItem = (itemId) => {
+    // Remover un item del cart por usando su id
+    setCartList(cartList.filter((item) => item.id !== itemId));
+    costTotal(cartList.filter((item) => item.id !== itemId));
+  };
+  const clear = () => {
+    setCartList([]);
+  }; // Remover todos los items
+  const isInCart = (id) => {
+    return cartList.filter((item) => item.id === parseInt(id)).length > 0;
+  };
+  const costTotal = (list) => {
+    console.log(list);
+    if (list.length > 0) {
+      let costos = list.map((item) => item.price * item.quantity);
+      settotals(
+        costos.reduce((a, b) => {
+          return a + b;
+        })
+      );
+    } else {
+      settotals(0);
     }
-    const clear = ()=>{ setCartList([])} // Remover todos los items
-    const isInCart = (id) =>{
-        return cartList.filter(item=> item.id === parseInt(id)).length > 0
-    }
-    const costTotal = (list)=>{
-        console.log(list)
-        if(list.length>0){
-            let costos = list.map((item)=>(
-                item.price*item.quantity
-            ))
-            settotals( costos.reduce((a, b)=>{ return a + b}))
-        }else{
-            settotals(0)
-        }
-    }
-    //bagde items
-    const itemCartQty =()=>{
-        let cartQty = cartList.map((producto)=> producto.quantity )
-        return(cartQty.reduce((acumQtys, quantity )=> acumQtys+=quantity,0))
-    }
-    
-    return(
-        <CartContext.Provider value={{cartList,totals, addItem, removeItem, clear, isInCart ,costTotal , itemCartQty}}>
-            {children}
-        </CartContext.Provider>
-    )
-}
+  };
+  //bagde items
+  const itemCartQty = () => {
+    let cartQty = cartList.map((producto) => producto.quantity);
+    return cartQty.reduce((acumQtys, quantity) => (acumQtys += quantity), 0);
+  };
+
+  return (
+    <CartContext.Provider
+      value={{
+        cartList,
+        totals,
+        addItem,
+        removeItem,
+        clear,
+        isInCart,
+        costTotal,
+        itemCartQty
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
 export default CartContextProvider;
