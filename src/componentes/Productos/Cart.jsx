a37775a291eef26efe5cc64795bf2b94c49fb4b3
Caro Arias <arias.carolina.ayelen@gmail.com>
2022-10-04 13:46:39 -0300

    se realizan cambios esteticos y se agregan nuevas funcionalidades a cart

--- src/componentes/Productos/Cart.jsx
+++ src/componentes/Productos/Cart.jsx
@@ -12,7 +12,8 @@ const Cart = () => {
   const [isEmpty, setIsEmpty] = useState(true);
 
   useEffect(() => {
-    setTotalCost(ctx.costTotal);
+    setTotalCost(ctx.costTotal)
+    
   }, [ctx]);
 
   const createOrder = async () => {
@@ -36,40 +37,44 @@ const Cart = () => {
     //se crea tickets ,en la base de datos
     const newOrderRef = doc(collection(db, 'tickets'));
      await setDoc(newOrderRef, order); 
-    
      alert(
       "Su orden ha sido procesada! Su numero de orden es " + newOrderRef.id
     );
+    //se vacia el carrito
+    ctx.clear()
   };
+  useEffect(() => {
+    ctx.cartList.length === 0 ? setIsEmpty(true) : setIsEmpty(false)
+}, [ctx.cartList])
   return (
     <main>
       <h1 className="text-center">Esta es su orden 🛒</h1>
       <section className="row mx-0">
         <div className="col-12 row">
           <Link to="/">
-            <button type="button" className="btn py-0 ">
-              Continuar comprando
+            <button type="button" className="btn py-0 font2">
+              Volver a los productos
             </button>
           </Link>
 
           <div className="col-6 d-flex justify-content-end">
-            <button type="button" className="btn py-0 " onClick={ctx.clear}>
+            <button type="button" className="btn py-0 font2" onClick={ctx.clear}>
               Eliminar compra
             </button>
           </div>
         </div>
         <div className="col-8 ">
           {
-          
+            isEmpty ? <p className="text-center m-5 fs-4">Ingrese algun producto <box-icon type='solid' name='cart-add'></box-icon></p> //en caso que este vacio se muestra el texto
             //Se mapea el contenido y se imprime en la lista
-            ctx.cartList.map((item) => (
+            :ctx.cartList.map((item) => (
               <ItemCart product={item} key={item.id} />
             ))
           }
         </div>
         <div className="row col-3">
-          <h3 className="text-center ">Su orden</h3>
-          <p className="col-6">
+          <h3 className="ms-5 font1 ">Su orden</h3>
+          <p className="col-6 font2">
             SubTotal:
             <br />
             IVA:
@@ -85,8 +90,8 @@ const Cart = () => {
             <br />
             <strong>${totalCost}</strong>
           </p>
-          <div className="text-center">
-            <button onClick={createOrder} type="button" className="btn p-2 ">
+          <div className="ms-5">
+            <button onClick={createOrder} type="button" className="btn">
               Finalizar Compra
             </button>
           </div>
